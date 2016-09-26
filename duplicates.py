bb5b8d36aab35515955cd73e183dd00fdc27e7e8
Your Name <you@example.com>
2016-09-26 22:30:06 +0300

    Реализация поставленной задачи

--- duplicates.py
+++ duplicates.py
@@ -1,26 +1,40 @@
 import os
+import sys
 
 
-def are_files_duplicates(file_path1, file_path_2):
-    pass
+def are_files_duplicates(file_path):
+    list_files = []
+    for f_path, all_dirs, all_files in os.walk(file_path):
+        for file_data in all_files:
+            full_path = os.path.join(f_path, file_data)
+            about_file = []
+            file_dir = os.path.dirname(full_path)
+            file_name = os.path.basename(full_path)
+            file_size = os.path.getsize(full_path)
+            for file_info in list_files:
+                if (file_name == file_info[1] and file_size == file_info[2]):
+                    print('Дубликат в папках:', file_info[0], 'и',
+                          file_dir, '. Файл:', file_name, ',', file_size, 'b.')
+                    break
+            else:
+                about_file.append(file_dir)
+                about_file.append(file_name)
+                about_file.append(file_size)
+                list_files.append(about_file)
 
+    return None
 
-def get_all_files(path):
-    tuple_path_files = ()
-    about_file = ()
-    for files_and_folders in os.listdir(path):
-        full_path = os.path.join(path, files_and_folders)
-        if os.path.isfile(full_path):
-            about_file.append(os.path.dirname(full_path))
-            about_file.append(files_and_folders)
-            about_file.append(os.path.getsize(full_path))
-            tuple_path_files.append(about_file)
-            about_file.clear()
-        else:
-            get_all_files(full_path)
-    return tuple_path_files
 
 if __name__ == '__main__':
-    pass
-
-print(get_all_files("/home/kodi/python"))
+    if len(sys.argv) > 1:
+        if sys.argv[1] == '--help':
+            print("Скрипт просматривает все файлы и ищет дубликаты в папке.")
+            print("Дубликаты – это два файла с одинаковым именем и размером.")
+            print("Введите в терминале: python3.5 dublicates.py your_path")
+        else:
+            if os.path.isdir(sys.argv[1]) is False:
+                print("Указанная папка не обнаружена!")
+            else:
+                are_files_duplicates(sys.argv[1])
+    else:
+        print("Не задана папка для поиска дубликатов!")
