68a9986bf6d65ea120bf7ca395cf9b10828913cd
Your Name <you@example.com>
2016-09-30 19:57:26 +0300

    внесены исправления

--- duplicates.py
+++ duplicates.py
@@ -1,42 +1,56 @@
 import os
 import sys
+import argparse
+
+def get_all_files(path):
+    all_files = []
+    for dirpath, dirnames, filenames in os.walk(path):
+        for filename in filenames:
+            full_path = os.path.join(dirpath, filename)
+            all_files.append(full_path)
+    return all_files
 
 
-def are_files_duplicates(file_path):
-    list_files = []
-    have_dublicate = False
-    for f_path, all_dirs, all_files in os.walk(file_path):
-        for file_data in all_files:
-            full_path = os.path.join(f_path, file_data)
-            about_file = []
-            file_dir = os.path.dirname(full_path)
-            file_name = os.path.basename(full_path)
-            file_size = os.path.getsize(full_path)
-            for file_info in list_files:
-                if (file_name == file_info[1] and file_size == file_info[2]):
-                    print('Дубликат в папках:', file_info[0], 'и',
-                          file_dir, '. Файл:', file_name, ',', file_size, 'b.')
-                    have_dublicate = True
-                    break
-            else:
-                about_file.append(file_dir)
-                about_file.append(file_name)
-                about_file.append(file_size)
-                list_files.append(about_file)
-    return have_dublicate
+def are_files_duplicates(files):
+    list_files = {}
+    dubl_files = {}
+    for filename in files:
+        file_info = (os.path.basename(filename), os.path.getsize(filename))
+        if not file_info in list_files.keys():
+            list_files[file_info] = os.path.dirname(filename)
+        else:
+            dubl_files[file_info] = os.path.dirname(filename)
+    return dubl_files
+
+
+def check_folder(path):
+    return os.path.isdir(path)
+
+def createParser():
+    parser = argparse.ArgumentParser(usage='%(prog)s [аргументы]',
+                                     description="Поиск дубликатов в" \
+                                     "папке с помощью %(prog)s")
+    parser._positionals.title = 'Обязательные аргументы'
+    parser._optionals.title = 'Дополнительные аргументы'
+    parser._actions[0].help = 'Показать эту справку и выйти.'
+    parser.add_argument("dirpath", help="Путь к папке")
+    return parser
 
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        if sys.argv[1] == '--help':
-            print("Скрипт просматривает все файлы и ищет дубликаты в папке.")
-            print("Дубликаты – это два файла с одинаковым именем и размером.")
-            print("Введите в терминале: python3.5 dublicates.py your_path")
+
+    parser = createParser()
+    namespace = parser.parse_args()
+    dir_path = namespace.dirpath
+
+    if check_folder(dir_path):
+        all_files = get_all_files(dir_path)
+        all_dubles = are_files_duplicates(all_files)
+        if not len(all_dubles) == 0:
+            for dubl_file in all_dubles:
+                print(dubl_file,"- дубликат в папке:",
+                                            all_dubles.get(dubl_file))
         else:
-            if os.path.isdir(sys.argv[1]) is False:
-                print("Указанная папка не обнаружена!")
-            else:
-                if not are_files_duplicates(sys.argv[1]):
-                    print("Дубликаты в папке",sys.argv[1],"не найдены")
+            print("Дубликаты в папке","не найдены")
     else:
-        print("Не задана папка для поиска дубликатов!")
+        print("Указанная папка не обнаружена!")
